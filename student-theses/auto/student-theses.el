e8de11dbe0cc7ed963a7b3f476aed98c481e5b8d
Faisal Qureshi <faisal.qureshi@ontariotechu.ca>
2025-10-04 14:09:45 -0400

    Adding fall 2025 courses.

---
+++ student-theses/auto/student-theses.el
@@ -0,0 +1,36 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "student-theses"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Rezasoltani"
+    "Mantripragada"
+    "Lycksam"
+    "Alqaysi"
+    "Helala"
+    "Amjad"
+    "Chan"
+    "Nadeem"
+    "Ejaz"
+    "Tabaraki"
+    "Karia"
+    "Dehghani"
+    "Jafarian"
+    "Talpur"
+    "Lombardo"
+    "Baeenh"
+    "Joseph"
+    "Taylor"
+    "Nemirovsky"
+    "Shanks"
+    "Reina"
+    "Stadler"
+    "Starzyk"
+    "Parvin"
+    "Wang"
+    "Murray"
+    "Little"
+    "Lobo"))
+ '(or :bibtex :latex))
+
